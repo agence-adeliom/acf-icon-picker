69619573047c6043eff1a271bf5342e6c0536c6e
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2025-02-10 20:05:14 +0100

    fix: prefix class name by adeliom to avoid conflicts

--- src/fields/acf-icon-picker.php
+++ src/fields/acf-icon-picker.php
@@ -1,12 +1,14 @@
 <?php
 
-if( ! defined( 'ABSPATH' ) ) exit;
+if (! defined('ABSPATH')) exit;
 
-if( !class_exists('acf_field_icon_picker') ) :
+if (!class_exists('adlm_acf_field_icon_picker')) :
 
-    class acf_field_icon_picker extends acf_field {
+    class adlm_acf_field_icon_picker extends acf_field
+    {
 
-        function __construct( $settings ) {
+        function __construct($settings)
+        {
 
             $this->name = 'icon-picker';
 
@@ -15,40 +17,40 @@ if( !class_exists('acf_field_icon_picker') ) :
             $this->category = 'choice';
 
             $this->defaults = array(
-                'initial_value'	=> '',
+                'initial_value'    => '',
             );
 
             $this->l10n = array(
-                'error'	=> __('Error!', 'acf-icon-picker'),
+                'error'    => __('Error!', 'acf-icon-picker'),
             );
 
             $this->settings = $settings;
 
-            $this->imgPicker = apply_filters( 'acf_icon_image', false);
-            $this->imgFormat = apply_filters( 'acf_icon_image_format', "svg");
+            $this->imgPicker = apply_filters('acf_icon_image', false);
+            $this->imgFormat = apply_filters('acf_icon_image_format', "svg");
 
             $this->filename = apply_filters('acf_icon_filename', 'style.css');
 
-            if($this->imgPicker) {
-                $this->path_suffix = apply_filters( 'acf_icon_path_suffix', 'assets/images/icons/' );
+            if ($this->imgPicker) {
+                $this->path_suffix = apply_filters('acf_icon_path_suffix', 'assets/images/icons/');
             } else {
-                $this->path_suffix = apply_filters( 'acf_icon_path_suffix', 'assets/fonts/icons/' );
+                $this->path_suffix = apply_filters('acf_icon_path_suffix', 'assets/fonts/icons/');
             }
 
-            $this->path = apply_filters( 'acf_icon_path', $this->settings['path'] ) . $this->path_suffix;
+            $this->path = apply_filters('acf_icon_path', $this->settings['path']) . $this->path_suffix;
 
-            $this->url = apply_filters( 'acf_icon_url', $this->settings['url'] ) . $this->path_suffix;
+            $this->url = apply_filters('acf_icon_url', $this->settings['url']) . $this->path_suffix;
 
             $priority_dir_lookup = get_stylesheet_directory() . '/' . $this->path_suffix;
 
-            if ( file_exists( $priority_dir_lookup ) ) {
+            if (file_exists($priority_dir_lookup)) {
                 $this->path = $priority_dir_lookup;
                 $this->url = get_stylesheet_directory_uri() . '/' . $this->path_suffix;
             }
 
             $this->icons = array();
 
-            if($this->imgPicker) {
+            if ($this->imgPicker) {
                 $files = array_diff(scandir($this->path), array('.', '..'));
                 foreach ($files as $file) {
                     if (pathinfo($file, PATHINFO_EXTENSION) == $this->imgFormat) {
@@ -69,84 +71,85 @@ if( !class_exists('acf_field_icon_picker') ) :
         {
             $results = array();
             preg_match_all('/(?ims)([a-z0-9\s\,\.\:#_\-@*()\[\]"=]+)\{([^\}]*)\}/', $css, $arr);
-            foreach ($arr[0] as $i => $x)
-            {
+            foreach ($arr[0] as $i => $x) {
                 $selector = trim($arr[1][$i]);
                 $selector = str_replace('.', '', $selector);
-                if(str_starts_with($selector, 'icon-') && str_ends_with($selector, ':before')){
+                if (str_starts_with($selector, 'icon-') && str_ends_with($selector, ':before')) {
                     $selector = str_replace(':before', '', $selector);
-                    $results[] = $selector ;
+                    $results[] = $selector;
                 }
             }
             return $results;
         }
 
-        function render_field_settings( $field ) {
-            acf_render_field_setting( $field, array(
-                'label'			=> __('Icônes par défaut'),
-                'type'			=> 'text',
-                'name'			=> 'icons',
+        function render_field_settings($field)
+        {
+            acf_render_field_setting($field, array(
+                'label'            => __('Icônes par défaut'),
+                'type'            => 'text',
+                'name'            => 'icons',
             ));
         }
 
-        function render_field( $field ) {
+        function render_field($field)
+        {
             $icons = !empty($field['icons']) ? $field['icons'] : '';
             $input_icon = $field['value'] != "" ? $field['value'] : $field['initial_value'];
-            ?>
+?>
             <div class="acf-icon-picker">
                 <div class="acf-icon-picker__img">
                     <?php
                     if ($input_icon) {
                         echo '<div class="acf-icon-picker__icon">';
-                        if($this->imgPicker) {
+                        if ($this->imgPicker) {
                             $svg = get_stylesheet_directory_uri() . '/' . $this->path_suffix . $input_icon . '.' . $this->imgFormat;
-                            echo '<img src="'.$svg.'" alt=""/>';
+                            echo '<img src="' . $svg . '" alt=""/>';
                         } else {
-                            echo '<i class="'. $input_icon .'"></i>';
+                            echo '<i class="' . $input_icon . '"></i>';
                         }
                         echo '</div>';
-                    }else{
+                    } else {
                         echo '<div class="acf-icon-picker__icon">';
                         echo '<span class="acf-icon-picker__icon--span">Ajouter</span>';
                         echo '</div>';
                     }
                     ?>
-                    <input type="hidden" readonly class="icon-value" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($input_icon) ?>"/>
+                    <input type="hidden" readonly class="icon-value" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo esc_attr($input_icon) ?>" />
                     <input type="hidden" readonly class="icons-list" value='<?php echo json_encode($icons) ?>' />
                 </div>
-                <?php if ( $field['required' ] == false ) { ?>
+                <?php if ($field['required'] == false) { ?>
                     <span class="acf-icon-picker__remove">
-						Supprimer
-					</span>
+                        Supprimer
+                    </span>
                 <?php } ?>
             </div>
-            <?php
+<?php
         }
 
-        function input_admin_enqueue_scripts() {
+        function input_admin_enqueue_scripts()
+        {
 
             $url = $this->settings['url'];
             $version = $this->settings['version'];
-		
-	    wp_register_style( 'acf-input-icon-picker', "{$url}/src/assets/css/input.css", array('acf-input'), $version );
-            wp_register_style( 'acf-input-icon-picker-icomoon', "{$this->url}/style.css", array('acf-input'), $version );
+
+            wp_register_style('acf-input-icon-picker', "{$url}/src/assets/css/input.css", array('acf-input'), $version);
+            wp_register_style('acf-input-icon-picker-icomoon', "{$this->url}/style.css", array('acf-input'), $version);
             wp_enqueue_style('acf-input-icon-picker');
             wp_enqueue_style('acf-input-icon-picker-icomoon');
 
-            if($this->imgPicker) {
-                wp_register_script( 'acf-input-icon-picker', "{$url}/src/assets/js/input-svg.js", array('acf-input'), $version );
+            if ($this->imgPicker) {
+                wp_register_script('acf-input-icon-picker', "{$url}/src/assets/js/input-svg.js", array('acf-input'), $version);
                 wp_enqueue_script('acf-input-icon-picker');
 
-                wp_localize_script( 'acf-input-icon-picker', 'iv', array(
+                wp_localize_script('acf-input-icon-picker', 'iv', array(
                     'path' => $this->url,
                     'svgs' => $this->icons,
-                    'no_icons_msg' => sprintf( esc_html__('Pour ajouter des icônes (svg), sauvegarder le contenu de votre dossier images dans /%s de votre thème.', 'acf-icon-picker'), $this->path_suffix)
-                ) );
-            }
-            else {
+                    'no_icons_msg' => sprintf(esc_html__('Pour ajouter des icônes (svg), sauvegarder le contenu de votre dossier images dans /%s de votre thème.', 'acf-icon-picker'), $this->path_suffix)
+                ));
+            } else {
                 $css_url = $this->path . $this->filename;
 
-                if(!file_exists($css_url)) {
+                if (!file_exists($css_url)) {
                     return;
                 }
 
@@ -154,18 +157,18 @@ if( !class_exists('acf_field_icon_picker') ) :
 
                 $this->icons = self::BreakCSS($css);
 
-                wp_register_script( 'acf-input-icon-picker', "{$url}/src/assets/js/input.js", array('acf-input'), $version );
+                wp_register_script('acf-input-icon-picker', "{$url}/src/assets/js/input.js", array('acf-input'), $version);
                 wp_enqueue_script('acf-input-icon-picker');
 
-                wp_localize_script( 'acf-input-icon-picker', 'iv', array(
+                wp_localize_script('acf-input-icon-picker', 'iv', array(
                     'path' => $this->url,
                     'icons' => $this->icons,
-                    'no_icons_msg' => sprintf( esc_html__('Pour ajouter des icônes, sauvegarder le contenu de votre dossier dans /%s de votre thème.', 'acf-icon-picker'), $this->path_suffix)
-                ) );
+                    'no_icons_msg' => sprintf(esc_html__('Pour ajouter des icônes, sauvegarder le contenu de votre dossier dans /%s de votre thème.', 'acf-icon-picker'), $this->path_suffix)
+                ));
             }
         }
     }
-    new acf_field_icon_picker( $this->settings );
+    new adlm_acf_field_icon_picker($this->settings);
 
 endif;
 
